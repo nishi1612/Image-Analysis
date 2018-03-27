0efd5014739544b5fb21d1cc9f5be8c7c15fed3b
Nishi Doshi <doshinishi1612@gmail.com>
2018-03-27 23:41:02 +0530

    Add files via upload

---
+++ image_processing.py
@@ -0,0 +1,58 @@
+from PIL import Image
+
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+def flag_value(flag):
+    flag = flag+1
+    flag = flag%4
+    return flag
+
+k = 1984
+b = '2016_crop.jpg'
+
+while(k<2016):
+    a = str(k)
+    a = a + '_crop.jpg'
+    im1 = Image.open(a, 'r')
+    im2 = Image.open(b,'r')
+    img2 = Image.new( im2.mode, im2.size)
+    pixelsNew2 = img2.load()
+    flag = 0
+    for i in range(img2.size[0]):
+        for j in range(img2.size[1]):
+            if (cmp(im1.getpixel((i,j)),im2.getpixel((i,j)))>=1 and i<img2.size[0]/2 and j<img2.size[1]/2):#pix_val1([i][j]),pix_val2([i][j]))==0):
+                if(flag==0):
+                    pixelsNew2[i,j] = (255,0,0)
+                    flag = flag_value(flag)
+                else:
+                    pixelsNew2[i,j] = im2.getpixel((i,j))
+                    flag = flag_value(flag)
+            elif(cmp(im1.getpixel((i,j)),im2.getpixel((i,j)))>=1 and i<img2.size[0]/2 and j>img2.size[1]/2):
+                if(flag==0):
+                    pixelsNew2[i,j] = (0,0,255)
+                    flag = flag_value(flag)
+                else:
+                    pixelsNew2[i,j] = im2.getpixel((i,j))
+                    flag = flag_value(flag)
+            elif (cmp(im1.getpixel((i,j)),im2.getpixel((i,j)))>=1 and i>img2.size[0]/2 and j>=img2.size[1]/2):
+                if(flag==0):
+                    pixelsNew2[i,j] = (255,0,0)
+                    flag = flag_value(flag)
+                else:
+                    pixelsNew2[i,j] = im2.getpixel((i,j))
+                    flag = flag_value(flag)
+            elif(cmp(im1.getpixel((i,j)),im2.getpixel((i,j)))>=1 and i>img2.size[0]/2 and j<img2.size[1]/2):
+                if(flag==0):
+                    pixelsNew2[i,j] = (0,0,255)
+                    flag = flag_value(flag)
+                else:
+                    pixelsNew2[i,j] = im2.getpixel((i,j))
+                    flag = flag_value(flag)
+            else:
+                pixelsNew2[i,j] = im2.getpixel((i,j))
+    c = str(k)
+    c = c + '_2016.jpg'
+    img2.save(c)
+    print(c + " image processed")
+    k = k + 1
